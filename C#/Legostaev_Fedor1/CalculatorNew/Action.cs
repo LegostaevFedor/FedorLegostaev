57f3f659bbc31e94d272b18e1ced9fda6e58bfad
LegostaevFedor <87949549+LegostaevFedor@users.noreply.github.com>
2022-01-09 18:49:58 +0300

    C#
    C#

---
+++ C#/Legostaev_Fedor1/CalculatorNew/Action.cs
@@ -0,0 +1,17 @@
+﻿namespace CalculatorNew
+{
+    public enum Action
+    {
+        Plus = 1,
+
+        Minus = 2,
+
+        Divide = 3,
+
+        Multiplication = 4,
+
+        Stepen = 5,
+
+        Sravnenie = 6
+    }
+}
