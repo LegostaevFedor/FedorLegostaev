ccf28a1e11dd86a50792341928a771b3ce840799
LegostaevFedor <87949549+LegostaevFedor@users.noreply.github.com>
2022-01-22 20:23:05 +0300

    C#

---
+++ C#/Legostaev_Fedor3/Array2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] massiv = new int[3, 3] { { 155, 3, 21 }, { 6, 15, 4 }, { 78, 11, 9 } };
+            
+            for (int i = 0; i < 3; i++)
+            {
+                int max = massiv[i, 0];
+
+                for (int j = 1; j < 3; j++)
+                {
+                    if (massiv[i, j] > max)
+                    {
+                        max = massiv[i, j];
+                    }
+                }
+
+                Console.WriteLine("Максимальное значение {0} ряда чисел : {1}", i+1, max);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Legostaev_Fedor3/ConsoleApp9/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] massiv = new int[6];
+            int perezap;
+
+            Console.WriteLine("Введите значения массива:\n");
+
+            for (int i = 0; i < massiv.Length; i++)
+            {
+                Console.Write("{0} элемент массива: ", i + 1);
+                massiv[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int k = 0; k < massiv.Length-1; k++)
+            {
+                for (int h = k + 1; h < massiv.Length; h++)
+                {
+                    if (massiv[k] < massiv[h])
+                    {
+                        perezap = massiv[k];
+                        massiv[k] = massiv[h];
+                        massiv[h] = perezap;
+
+                    }
+                }
+            }
+
+            Console.WriteLine("\nМассив в порядке убывания:\n");
+
+            for (int l = 0; l < massiv.Length; l++)
+            {
+                Console.Write(massiv[l] + " ");
+            }
+
+            Console.ReadKey();
+            /*
+            Array.Sort(massiv);
+            Array.Reverse(massiv);
+            */
+        }
+    }
+}
